10b082926194d9736d9ffdfca8064bac84bc1da8
tirtahakimpambudhi <awebthp@gmail.com>
2025-08-21 11:53:42 +0700

    chore: Update lookup.go files

--- db/seed/lookup.go
+++ db/seed/lookup.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"time"
 
+	"github.com/go-faker/faker/v4"
 	"github.com/thoriqwildan/aino-medical-be/internal/entity"
+	"github.com/thoriqwildan/aino-medical-be/internal/helper"
 	"gorm.io/gorm"
 )
 
@@ -137,5 +139,116 @@ func SeedFamilyMemberAndEmployee(db *gorm.DB) {
 		} else {
 			log.Printf("Employee %s already exists, skipping.\n", existingEmpl.Name)
 		}
+	}
+}
+
+func SeedClaimsAndPatients(db *gorm.DB) {
+	for i := 0; i < 100; i++ {
+		genders := []string{"male", "female"}
+		relationshipTypes := []string{"husband", "wife", "child", "father", "mother"}
+		benefits := make([]entity.Benefit, 10)
+		if errFindBenefits := db.Find(&benefits).Limit(10).Error; errFindBenefits != nil {
+			log.Fatalf("Error find benefits: %v\n", errFindBenefits)
+		}
+		planTypes := make([]entity.PlanType, 4)
+		if errFindPlanTypes := db.Find(&planTypes).Limit(4).Error; errFindPlanTypes != nil {
+			log.Fatalf("Error find planTypes: %v\n", errFindPlanTypes)
+		}
+
+		transactionTypes := make([]entity.TransactionType, 4)
+		if errFindTransactionTypes := db.Find(&transactionTypes).Limit(4).Error; errFindTransactionTypes != nil {
+			log.Fatalf("Error find transactionTypes: %v\n", errFindTransactionTypes)
+		}
+
+		departments := make([]entity.Department, 4)
+		if errFindDepartment := db.Find(&departments).Limit(4).Error; errFindDepartment != nil {
+			log.Fatalf("Error find departments: %v\n", errFindDepartment)
+		}
+
+		randomBenefit := helper.RandomInt(0, len(benefits)-1)
+		randomDepartment := helper.RandomInt(0, len(departments)-1)
+		randomPlanType := helper.RandomInt(0, len(planTypes)-1)
+		randomTransactionType := helper.RandomInt(0, len(transactionTypes)-1)
+
+		fakeDate, errParseDate := time.Parse("2006-01-02", faker.Date())
+		if errParseDate != nil {
+			log.Fatalf("Error seeding fakeDate: %v\n", errParseDate)
+		}
+		employee := entity.Employee{
+			Name:         faker.Name(),
+			DepartmentID: departments[randomDepartment].ID,
+			Position:     departments[randomDepartment].Name,
+			Email:        faker.Email(),
+			Phone:        faker.Phonenumber(),
+			BirthDate:    fakeDate,
+			Gender:       entity.Genders(genders[helper.RandomInt(0, len(genders)-1)]),
+			PlanTypeID:   planTypes[randomPlanType].ID,
+			Dependence:   ptrString(faker.Word()),
+			BankNumber:   faker.CreditCardNumber,
+			ProRate:      helper.ProRateRemainingMonthsPercent(time.Now()),
+			JoinDate:     time.Now(),
+		}
+		if err := db.Create(&employee).Error; err != nil {
+			log.Fatalf("Error when seeding employee: %v\n", err)
+		}
+		patient := entity.Patient{
+			Name:       faker.Name(),
+			BirthDate:  fakeDate,
+			Gender:     entity.Genders(genders[helper.RandomInt(0, len(genders)-1)]),
+			PlanTypeID: planTypes[randomPlanType].ID,
+			EmployeeID: &employee.ID,
+			FamilyMember: &entity.FamilyMember{
+				Name:             faker.Name(),
+				RelationshipType: entity.RelationshipTypes(relationshipTypes[helper.RandomInt(0, len(relationshipTypes)-1)]),
+				PlanTypeID:       planTypes[randomPlanType].ID,
+				BirthDate:        fakeDate,
+				Gender:           entity.Genders(genders[helper.RandomInt(0, len(genders)-1)]),
+				EmployeeID:       employee.ID,
+			},
+		}
+		if err := db.Create(&patient).Error; err != nil {
+			log.Fatalf("Error when seeding patient: %v\n", err)
+		}
+		patientBenefit := entity.PatientBenefit{
+			PatientID:        patient.ID,
+			BenefitID:        benefits[randomBenefit].ID,
+			RemainingPlafond: benefits[randomBenefit].Plafond,
+			InitialPlafond:   benefits[randomBenefit].Plafond,
+			StartDate:        employee.JoinDate,
+			EndDate:          &employee.JoinDate,
+			Status:           entity.PatientBenefitStatusActive,
+		}
+		if err := db.Create(&patientBenefit).Error; err != nil {
+			log.Fatalf("Error when seeding patient: %v\n", err)
+		}
+		sla := []entity.SLA{entity.SLAOverdue, entity.SLAMeet}[helper.RandomInt(0, 1)]
+		claimStatus := []entity.TransactionStatus{entity.TransactionStatusSuccessful, entity.TransactionStatusFailed, entity.TransactionStatusPending}[helper.RandomInt(0, 2)]
+		db.Create(&entity.Claim{
+			PatientBenefitID: patientBenefit.ID,
+			PatientID:        patient.ID,
+			EmployeeID:       employee.ID,
+			ClaimAmount: func() float64 {
+				if benefits[randomBenefit].Plafond != nil {
+					return *benefits[randomBenefit].Plafond
+				}
+				return float64(helper.RandomInt(1000000, 10000000))
+			}(),
+			TransactionTypeID: &transactionTypes[randomTransactionType].ID,
+			TransactionDate:   ptrDate(time.Now()),
+			SubmissionDate:    ptrDate(time.Now()),
+			SLA:               &sla,
+			ApprovedAmount: func() *float64 {
+				if benefits[randomBenefit].Plafond != nil {
+					return benefits[randomBenefit].Plafond
+				}
+				return ptrFloat64(float64(helper.RandomInt(1000000, 10000000)))
+			}(),
+			ClaimStatus:         entity.ClaimStatusOnPlafond,
+			MedicalFacilityName: ptrString(faker.ChineseName()),
+			City:                ptrString("Daerah Istimewa Yogyakarta"),
+			Diagnosis:           ptrString(faker.Word()),
+			DocLink:             ptrString(faker.URL()),
+			TransactionStatus:   claimStatus,
+		})
 	}
 }
