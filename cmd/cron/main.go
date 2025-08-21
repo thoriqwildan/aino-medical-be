6ee644cddb630b17d83c838699142d5b84308fb4
tirtahakimpambudhi <awebthp@gmail.com>
2025-08-21 16:08:51 +0700

    chore: Add main.go files

---
+++ cmd/cron/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/robfig/cron/v3"
+	"github.com/thoriqwildan/aino-medical-be/internal/config"
+	"github.com/thoriqwildan/aino-medical-be/internal/entity"
+)
+
+func main() {
+	viperConfig := config.NewViper()
+	logger := config.NewLogger(viperConfig)
+	db := config.NewDatabase(viperConfig, logger)
+	c := cron.New()
+	logger.Info("Starting cron job")
+	c.AddFunc("0 0 * * *", func() {
+		logger.Info("Do Cron Job Check Employee Pro Rate")
+		var employees []*entity.Employee
+		if err := db.Find(&employees).Error; err != nil {
+			logger.Error("Error when find the all employee", err)
+			log.Fatal(err)
+		}
+
+		for _, employee := range employees {
+			now := time.Now()
+
+			join := employee.JoinDate
+			if join.Location() != now.Location() {
+				now = now.In(join.Location())
+			}
+
+			oneYearElapsed := !now.Before(join.AddDate(1, 0, 0))
+
+			if !join.IsZero() && oneYearElapsed {
+				if err := db.Model(&entity.Employee{}).
+					Where("id = ?", employee.ID).
+					Update("pro_rate", 0).Error; err != nil {
+					logger.Errorf("set pro_rate=0 for employee %d: %v", employee.ID, err)
+				}
+			}
+
+		}
+	}) // every 00:00
+
+	c.Run()
+}
