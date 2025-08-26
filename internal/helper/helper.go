4b2c985b7552bb9ec393988baf98a018d3adfd95
tirtahakimpambudhi <awebthp@gmail.com>
2025-08-26 11:53:49 +0700

    chore: Update helper.go files

--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -1,8 +1,16 @@
 package helper
 
+import "math/rand"
+
 func ToNullString(s string) *string {
 	if s == "" {
 		return nil
 	}
 	return &s
+}
+func RandomInt(min, max int) int {
+	if min > max {
+		min, max = max, min
+	}
+	return rand.Intn(max-min+1) + min
 }
