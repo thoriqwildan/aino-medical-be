9e869715dbb59089fc00984c537cea8c6db0d994
tirtahakimpambudhi <awebthp@gmail.com>
2025-08-28 15:08:02 +0700

    chore: Update benefit_converter.go files

--- internal/model/converter/benefit_converter.go
+++ internal/model/converter/benefit_converter.go
@@ -1,25 +1,37 @@
 package converter
 
 import (
+	"fmt"
+
 	"github.com/thoriqwildan/aino-medical-be/internal/entity"
 	"github.com/thoriqwildan/aino-medical-be/internal/model"
 )
 
 func BenefitToResponse(benefit *entity.Benefit) *model.BenefitResponse {
-  response := &model.BenefitResponse{
-    ID:          benefit.ID,
-    Name:        benefit.Name,
-    Detail:      benefit.Detail,
-    Code:        benefit.Code,
-    Plafond:     &benefit.Plafond,
-    YearlyMax:   &benefit.YearlyMax,
-  }
+	response := &model.BenefitResponse{
+		ID:     benefit.ID,
+		Name:   benefit.Name,
+		Detail: benefit.Detail,
+		Code:   benefit.Code,
+	}
 
-  if benefit.PlanType.ID != 0 {
-    response.PlanType = *PlanTypeToResponse(&benefit.PlanType)
-  }
-  if benefit.LimitationType.ID != 0 {
-    response.LimitationType = *LimitationTypeToResponse(&benefit.LimitationType)
-  }
-  return response
+	if benefit.YearlyMax != nil {
+		response.YearlyMax = benefit.YearlyMax
+	} else {
+		response.YearlyMax = nil
+	}
+	if benefit.Plafond != nil {
+		response.Plafond = benefit.Plafond
+	} else {
+		benefit.Plafond = nil
+	}
+
+	if benefit.PlanType.ID != 0 {
+		response.PlanType = *PlanTypeToResponse(&benefit.PlanType)
+	}
+	if benefit.LimitationType.ID != 0 {
+		response.LimitationType = *LimitationTypeToResponse(&benefit.LimitationType)
+	}
+	fmt.Println(response)
+	return response
 }
