ec085d87bfce48b6462c67a6b44485d2d0b2d3f8
tirtahakimpambudhi <awebthp@gmail.com>
2025-08-28 11:14:57 +0700

    chore: Update model.go files

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,21 +1,27 @@
 package model
 
 type WebResponse[T any] struct {
-	Code				int    					`json:"code"`
-	Message 		string 					`json:"message"`
-	AccessToken string 					`json:"access_token,omitempty"`
-	Meta 				*PaginationPage `json:"meta,omitempty"`
-	Data 				*T    					`json:"data,omitempty"`
-	Errors 			any 						`json:"errors,omitempty"`
+	Code        int             `json:"code"`
+	Message     string          `json:"message"`
+	AccessToken string          `json:"access_token,omitempty"`
+	Meta        *PaginationPage `json:"meta,omitempty"`
+	Data        *T              `json:"data,omitempty"`
+	Errors      any             `json:"errors,omitempty"`
 }
 
 type PaginationPage struct {
-	Page 	int `json:"page"`
-	Limit 	int `json:"limit"`
+	Page  int `json:"page"`
+	Limit int `json:"limit"`
 	Total int `json:"total"`
 }
 
+type SearchPagingQuery struct {
+	SearchValue string `query:"search_value" json:"search_value"`
+	Page        int    `json:"page,omitempty" validate:"omitempty,numeric"`
+	Limit       int    `json:"limit,omitempty" validate:"omitempty,numeric"`
+}
+
 type PagingQuery struct {
-	Page int `json:"page,omitempty" validate:"omitempty,numeric"`
-	Limit int `json:"limit,omitempty" validate:"omitempty,numeric"`
+	Page  int `json:"page,omitempty" query:"page" validate:"omitempty,numeric"`
+	Limit int `json:"limit,omitempty" query:"limit" validate:"omitempty,numeric"`
 }
