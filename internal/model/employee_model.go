1a8ca45154e7cb055f3a23ffe68f0adebc5efff6
tirtahakimpambudhi <awebthp@gmail.com>
2025-08-30 15:08:05 +0700

    chore: Update employee_model.go files

--- internal/model/employee_model.go
+++ internal/model/employee_model.go
@@ -5,46 +5,47 @@ import (
 )
 
 type EmployeeRequest struct {
-	Name		 string `json:"name" validate:"required"`
-	DepartmentID uint   `json:"department_id" validate:"required"`
-	Position		 string `json:"position" validate:"required"`
-	Email		 string `json:"email" validate:"required,email"`
-	Phone		 string `json:"phone" validate:"required"`
-	BirthDate	 helper.CustomDate `json:"birth_date" validate:"required"`
-	Gender 	 string `json:"gender" validate:"required,oneof=male female"`
-	PlanTypeID	 uint   `json:"plan_type_id" validate:"required"`
-	Dependences string `json:"dependence,omitempty" validate:"omitempty"`
-	BankNumber	 string `json:"bank_number" validate:"required"`
-	JoinDate	 helper.CustomDate `json:"join_date" validate:"required"`
+	Name         string            `json:"name" validate:"required"`
+	DepartmentID uint              `json:"department_id" validate:"required"`
+	Position     string            `json:"position" validate:"required"`
+	Email        string            `json:"email" validate:"required,email"`
+	Phone        string            `json:"phone" validate:"required"`
+	BirthDate    helper.CustomDate `json:"birth_date" validate:"required"`
+	Gender       string            `json:"gender" validate:"required,oneof=male female"`
+	PlanTypeID   uint              `json:"plan_type_id" validate:"required"`
+	Dependences  string            `json:"dependence,omitempty" validate:"omitempty"`
+	BankNumber   string            `json:"bank_number" validate:"required"`
+	JoinDate     helper.CustomDate `json:"join_date" validate:"required"`
 }
 
 type EmployeeResponse struct {
-	ID            uint   `json:"id"`
-	Name		 			string `json:"name"`
-	Position		 	string `json:"position"`
-	Email		 			string `json:"email"`
-	Phone					string `json:"phone"`
-	BirthDate	 		helper.CustomDate `json:"birth_date"`
-	Gender 	 			string `json:"gender"`
-	Dependences 	string `json:"dependence,omitempty"`
-	BankNumber	 	string `json:"bank_number"`
-	JoinDate	 		helper.CustomDate `json:"join_date"`
-	PlanType	 		PlanTypeResponse   `json:"plan_type"`
-	Department   	DepartmentResponse   `json:"department"`
+	ID            uint                   `json:"id"`
+	Name          string                 `json:"name"`
+	Position      string                 `json:"position"`
+	Email         string                 `json:"email"`
+	Phone         string                 `json:"phone"`
+	BirthDate     helper.CustomDate      `json:"birth_date"`
+	Gender        string                 `json:"gender"`
+	Dependences   string                 `json:"dependence,omitempty"`
+	BankNumber    string                 `json:"bank_number"`
+	JoinDate      helper.CustomDate      `json:"join_date"`
+	ProRate       float64                `json:"pro_rate"`
+	PlanType      PlanTypeResponse       `json:"plan_type"`
+	Department    DepartmentResponse     `json:"department"`
 	FamilyMembers []FamilyMemberResponse `json:"family_members,omitempty"`
 }
 
 type UpdateEmployeeRequest struct {
-	ID            uint   `json:"id" validate:"required"`
-	Name		 string `json:"name" validate:"required"`
-	DepartmentID uint   `json:"department_id" validate:"required"`
-	Position		 string `json:"position" validate:"required"`
-	Email		 string `json:"email" validate:"required,email"`
-	Phone		 string `json:"phone" validate:"required"`
-	BirthDate	 helper.CustomDate `json:"birth_date" validate:"required"`
-	Gender 	 string `json:"gender" validate:"required,oneof=male female"`
-	PlanTypeID	 uint   `json:"plan_type_id" validate:"required"`
-	Dependences string `json:"dependence,omitempty" validate:"omitempty"`
-	BankNumber	 string `json:"bank_number" validate:"required"`
-	JoinDate	 helper.CustomDate `json:"join_date" validate:"required"`
+	ID           uint              `json:"id" validate:"required"`
+	Name         string            `json:"name" validate:"required"`
+	DepartmentID uint              `json:"department_id" validate:"required"`
+	Position     string            `json:"position" validate:"required"`
+	Email        string            `json:"email" validate:"required,email"`
+	Phone        string            `json:"phone" validate:"required"`
+	BirthDate    helper.CustomDate `json:"birth_date" validate:"required"`
+	Gender       string            `json:"gender" validate:"required,oneof=male female"`
+	PlanTypeID   uint              `json:"plan_type_id" validate:"required"`
+	Dependences  string            `json:"dependence,omitempty" validate:"omitempty"`
+	BankNumber   string            `json:"bank_number" validate:"required"`
+	JoinDate     helper.CustomDate `json:"join_date" validate:"required"`
 }
