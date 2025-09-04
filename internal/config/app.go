3d3701d7eb5a6cf48314552b32be8420753a527f
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-04 16:31:48 +0700

    chore: Update app.go files

--- internal/config/app.go
+++ internal/config/app.go
@@ -14,58 +14,58 @@ import (
 )
 
 type BootstrapConfig struct {
-	DB *gorm.DB
-	App *fiber.App
-	Log *logrus.Logger
+	DB       *gorm.DB
+	App      *fiber.App
+	Log      *logrus.Logger
 	Validate *validator.Validate
-	Config *viper.Viper
-	JWT *middleware.MiddlewareConfig
+	Config   *viper.Viper
+	JWT      *middleware.MiddlewareConfig
 }
 
 func Bootstrap(config *BootstrapConfig) {
 	userRepository := repository.NewUserRepository(config.Log)
 	transactionTypeRepository := repository.NewTransactionTypeRepository(config.Log)
 	planTypeRepository := repository.NewPlanTypeRepository(config.Log)
-	limitationTypeRepository := repository.NewLimitationTypeRepository(config.Log)
 	benefitRepository := repository.NewBenefitRepository(config.Log)
 	departmentRepository := repository.NewDepartmentRepository(config.Log)
 	employeeRepository := repository.NewEmployeeRepository(config.Log)
 	familyMemberRepository := repository.NewFamilyMemberRepository(config.Log)
 	claimRepository := repository.NewClaimRepository(config.Log)
 	patientBenefitRepository := repository.NewPatientBenefitRepository(config.Log)
+	yearlyBenefitClaimRepository := repository.NewYearlyBenefitClaimRepository(config.Log)
 
 	userUseCase := usecase.NewUserUseCase(config.DB, config.Log, userRepository, config.Validate)
 	transactionTypeUseCase := usecase.NewTransactionTypeUseCase(config.DB, config.Log, transactionTypeRepository, config.Validate)
 	planTypeUseCase := usecase.NewPlanTypeUseCase(config.DB, config.Log, planTypeRepository, config.Validate)
-	limitationTypeUseCase := usecase.NewLimitationTypeUseCase(limitationTypeRepository, config.DB, config.Log, config.Validate)
 	benefitUseCase := usecase.NewBenefitUseCase(benefitRepository, config.DB, config.Log, config.Validate)
 	departmentUseCase := usecase.NewDepartmentUseCase(departmentRepository, config.DB, config.Log, config.Validate)
 	employeeUseCase := usecase.NewEmployeeUseCase(config.DB, config.Log, employeeRepository, config.Validate)
 	familyMemberUseCase := usecase.NewFamilyMemberUseCase(familyMemberRepository, config.DB, config.Validate, config.Log)
 	claimUseCase := usecase.NewClaimUseCase(claimRepository, config.DB, config.Validate, config.Log, patientBenefitRepository, benefitRepository)
+	yearlyBenefitClaimUsecase := usecase.NewYearlyBenefitClaimUsecase(yearlyBenefitClaimRepository, config.Validate, config.DB, config.Log)
 
 	userController := http.NewUserController(userUseCase, config.Log, config.Config)
 	transactionTypeController := http.NewTransactionTypeController(transactionTypeUseCase, config.Log, config.Config)
 	planTypeController := http.NewPlanTypeController(planTypeUseCase, config.Log, config.Config)
-	limitationTypeController := http.NewLimitationTypeController(limitationTypeUseCase, config.Log, config.Config)
 	benefitController := http.NewBenefitController(benefitUseCase, config.Log)
 	departmentController := http.NewDepartmentController(departmentUseCase, config.Log)
 	employeeController := http.NewEmployeeController(employeeUseCase, config.Log)
 	familyMemberController := http.NewFamilyMemberController(familyMemberUseCase, config.Log, config.Config)
 	claimController := http.NewClaimController(claimUseCase, config.Log)
+	yearlyBenefitClaimController := http.NewYearlyBenefitClaimController(yearlyBenefitClaimUsecase, config.Log)
 
 	routeConfig := route.RouteConfig{
-		App: config.App,
-		JWT: config.JWT,
-		UserController: userController,
-		TransactionTypeController: transactionTypeController,
-		PlanTypeController: planTypeController,
-		LimitationTypeController: limitationTypeController,
-		BenefitController: benefitController,
-		DepartmentController: departmentController,
-		EmployeeController: employeeController,
-		FamilyMemberController: familyMemberController,
-		ClaimController: claimController,
+		App:                          config.App,
+		JWT:                          config.JWT,
+		UserController:               userController,
+		TransactionTypeController:    transactionTypeController,
+		PlanTypeController:           planTypeController,
+		BenefitController:            benefitController,
+		DepartmentController:         departmentController,
+		EmployeeController:           employeeController,
+		FamilyMemberController:       familyMemberController,
+		ClaimController:              claimController,
+		YearlyBenefitClaimController: yearlyBenefitClaimController,
 	}
 
 	routeConfig.Setup()
