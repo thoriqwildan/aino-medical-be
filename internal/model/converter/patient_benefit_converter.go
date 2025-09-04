f770f46df13ace83941ced69911659de58634ecb
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-04 10:46:50 +0700

    chore: Add patient_benefit_converter.go files

---
+++ internal/model/converter/patient_benefit_converter.go
@@ -0,0 +1,34 @@
+package converter
+
+import (
+	"github.com/thoriqwildan/aino-medical-be/internal/entity"
+	"github.com/thoriqwildan/aino-medical-be/internal/model"
+)
+
+func PatientBenefitToResponse(patientBenefit *entity.PatientBenefit) *model.PatientBenefitResponse {
+	response := &model.PatientBenefitResponse{
+		ID:               patientBenefit.ID,
+		PatientID:        patientBenefit.PatientID,
+		BenefitID:        patientBenefit.BenefitID,
+		RemainingPlafond: patientBenefit.RemainingPlafond,
+		InitialPlafond:   patientBenefit.InitialPlafond,
+		YearlyMax:        patientBenefit.YearlyMax,
+		StartDate:        patientBenefit.StartDate,
+		EndDate:          patientBenefit.EndDate,
+		Status:           string(patientBenefit.Status),
+		CreatedAt:        patientBenefit.CreatedAt,
+		UpdatedAt:        patientBenefit.UpdatedAt,
+	}
+	if patientBenefit.Patient.ID != 0 {
+		response.Patient = PatientToResponse(&patientBenefit.Patient)
+	}
+	if patientBenefit.Benefit.ID != 0 {
+		response.Benefit = BenefitToResponse(&patientBenefit.Benefit)
+	}
+	if len(patientBenefit.Claims) > 0 || patientBenefit.Claims != nil {
+		for _, claim := range patientBenefit.Claims {
+			response.Claims = append(response.Claims, ClaimToResponse(&claim))
+		}
+	}
+	return response
+}
