f7f46e50781e3b0ff8198a54434c6fd6fd4aab49
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-08 09:03:27 +0700

    chore: Update deparment_repository.go files

--- internal/repository/deparment_repository.go
+++ internal/repository/deparment_repository.go
@@ -31,10 +31,13 @@ func (r *DepartmentRepository) SearchDepartments(db *gorm.DB, request *model.Pag
 	if err := baseQuery.Count(&total).Error; err != nil {
 		return nil, 0, err
 	}
-
+	if request.Limit > 0 {
+		baseQuery = baseQuery.Limit(request.Limit)
+	}
+	if request.Page > 0 {
+		baseQuery = baseQuery.Offset((request.Page - 1) * request.Limit)
+	}
 	err := baseQuery.
-		Offset((request.Page - 1) * request.Limit).
-		Limit(request.Limit).
 		Find(&departments).Error
 	if err != nil {
 		return nil, 0, err
