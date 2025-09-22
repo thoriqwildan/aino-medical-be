80305cb8714917bee23c83a7a1d265e9254fc9b1
tirtahakimpambudhi <awebthp@gmail.com>
2025-09-22 16:32:04 +0700

    chore: Update patient_benefit_repository.go files

--- internal/repository/patient_benefit_repository.go
+++ internal/repository/patient_benefit_repository.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/thoriqwildan/aino-medical-be/internal/entity"
+	"github.com/thoriqwildan/aino-medical-be/internal/helper"
 	"gorm.io/gorm"
 )
 
@@ -22,42 +23,48 @@ func NewPatientBenefitRepository(log *logrus.Logger) *PatientBenefitRepository {
 
 func (r *PatientBenefitRepository) FindOrCreate(
 	db *gorm.DB,
-	patientID uint,
-	benefitID uint,
+	patient *entity.Patient,
+	benefit *entity.Benefit,
 	initialPlafond float64,
 	startDate time.Time,
+	prorate float64,
 ) (*entity.PatientBenefit, error) {
 	var patientBenefit entity.PatientBenefit
 
-	err := db.Where("patient_id = ? AND benefit_id = ?", patientID, benefitID).First(&patientBenefit).Error
+	err := db.Where("patient_id = ? AND benefit_id = ?", patient.ID, benefit.ID).First(&patientBenefit).Error
 
 	if err == nil {
-		r.Log.Printf("PatientBenefit found for PatientID: %d, BenefitID: %d", patientID, benefitID)
+		r.Log.Printf("PatientBenefit found for PatientID: %d, BenefitID: %d", patient.ID, benefit.ID)
 		return &patientBenefit, nil
 	}
 
 	if errors.Is(err, gorm.ErrRecordNotFound) {
-		r.Log.Printf("PatientBenefit not found for PatientID: %d, BenefitID: %d. Creating new record...", patientID, benefitID)
+		r.Log.Printf("PatientBenefit not found for PatientID: %d, BenefitID: %d. Creating new record...", patient.ID, benefit.ID)
 
 		newPatientBenefit := entity.PatientBenefit{
-			PatientID:        patientID,
-			BenefitID:        benefitID,
+			PatientID:        patient.ID,
+			BenefitID:        benefit.ID,
 			InitialPlafond:   &initialPlafond,
 			RemainingPlafond: &initialPlafond,
 			StartDate:        startDate,
 		}
 
+		if benefit.YearlyMax != nil {
+			yearlyMax := helper.CalculateProrateYearlyMax(*benefit.YearlyMax, prorate)
+			newPatientBenefit.YearlyMax = &yearlyMax
+		}
+
 		createErr := db.Create(&newPatientBenefit).Error
 		if createErr != nil {
-			r.Log.Printf("Error creating PatientBenefit for PatientID: %d, BenefitID: %d: %v", patientID, benefitID, createErr)
+			r.Log.Printf("Error creating PatientBenefit for PatientID: %d, BenefitID: %d: %v", patient.ID, benefit.ID, createErr)
 			return nil, createErr
 		}
 
-		r.Log.Printf("Successfully created new PatientBenefit with ID: %d for PatientID: %d, BenefitID: %d", newPatientBenefit.ID, patientID, benefitID)
+		r.Log.Printf("Successfully created new PatientBenefit with ID: %d for PatientID: %d, BenefitID: %d", newPatientBenefit.ID, patient.ID, benefit.ID)
 		return &newPatientBenefit, nil
 	}
 
-	r.Log.Printf("Database error checking PatientBenefit for PatientID: %d, BenefitID: %d: %v", patientID, benefitID, err)
+	r.Log.Printf("Database error checking PatientBenefit for PatientID: %d, BenefitID: %d: %v", patient.ID, benefit.ID, err)
 	return nil, err
 }
 
