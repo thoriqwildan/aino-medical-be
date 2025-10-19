7e4c719a23d1909dee5dcd6ef90f8ca4dac46394
tirtahakimpambudhi <awebthp@gmail.com>
2025-10-19 09:25:07 +0700

    chore: Update benefit_model.go files

--- internal/model/benefit_model.go
+++ internal/model/benefit_model.go
@@ -1,14 +1,13 @@
 package model
 
 type CreateBenefitRequest struct {
-	Name           string   `json:"name" validate:"required,min=3,max=255"`
-	PlanTypeID     uint     `json:"plan_type_id" validate:"required"`
-	YearlyClaimID  *uint    `json:"yearly_claim_id" validate:"omitempty"`
-	Detail         *string  `json:"detail,omitempty" validate:"omitempty,max=500"`
-	Code           string   `json:"code" validate:"required,min=3,max=50"`
-	LimitationType string   `json:"limitation_type" validate:"required,oneof='Per Year' 'Per Month' 'Per Day' 'Per Incident' 'Per Pregnancy'"`
-	Plafond        float64  `json:"plafond,omitempty" validate:"omitempty,numeric"`
-	YearlyMax      *float64 `json:"yearly_max" validate:"omitempty,numeric"`
+	Name           string  `json:"name" validate:"required,min=3,max=255"`
+	PlanTypeID     uint    `json:"plan_type_id" validate:"required"`
+	YearlyClaimID  *uint   `json:"yearly_claim_id" validate:"omitempty"`
+	Detail         *string `json:"detail,omitempty" validate:"omitempty,max=500"`
+	Code           string  `json:"code" validate:"required,min=3,max=50"`
+	LimitationType string  `json:"limitation_type" validate:"required,oneof='Per Year' 'Per Month' 'Per Day' 'Per Incident' 'Per Pregnancy'"`
+	Plafond        float64 `json:"plafond,omitempty" validate:"omitempty,numeric"`
 }
 
 type BenefitResponse struct {
@@ -17,8 +16,7 @@ type BenefitResponse struct {
 	Detail             *string                     `json:"detail,omitempty"`
 	Code               string                      `json:"code"`
 	Plafond            *float64                    `json:"plafond"`
-	YearlyMax          *float64                    `json:"yearly_max"`
-	RemainingPlafond   *float64                    `json:"remaining_plafond,omitempty"`
+	RemainingPlafond   *float64                    `json:"remaining_plafond"`
 	PlanType           PlanTypeResponse            `json:"plan_type"`
 	YearlyBenefitClaim *YearlyBenefitClaimResponse `json:"yearly_benefit_claim,omitempty"`
 	LimitationType     string                      `json:"limitation_type"`
