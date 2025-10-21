62b97b2d31530fddd30144d6c5e83493b3d1f1a6
tirtahakimpambudhi <awebthp@gmail.com>
2025-10-21 09:25:08 +0700

    chore: Update benefit_converter.go files

--- internal/model/converter/benefit_converter.go
+++ internal/model/converter/benefit_converter.go
@@ -16,11 +16,7 @@ func BenefitToResponse(benefit *entity.Benefit) *model.BenefitResponse {
 	if benefit.YearlyBenefitClaim != nil {
 		response.YearlyBenefitClaim = YearlyBenefitClaimToResponse(benefit.YearlyBenefitClaim)
 	}
-	if benefit.YearlyMax != nil {
-		response.YearlyMax = benefit.YearlyMax
-	} else {
-		response.YearlyMax = nil
-	}
+
 	if benefit.Plafond != nil {
 		response.Plafond = benefit.Plafond
 	} else {
@@ -29,6 +25,10 @@ func BenefitToResponse(benefit *entity.Benefit) *model.BenefitResponse {
 
 	if benefit.PlanType.ID != 0 {
 		response.PlanType = *PlanTypeToResponse(&benefit.PlanType)
+	}
+
+	if benefit.YearlyBenefitClaim != nil {
+		response.YearlyBenefitClaim = YearlyBenefitClaimToResponse(benefit.YearlyBenefitClaim)
 	}
 
 	return response
