542c6686ffa341e140f0e3aa9070c15eba5a2203
tirtahakimpambudhi <awebthp@gmail.com>
2025-10-22 09:25:08 +0700

    chore: Update claim_converter.go files

--- internal/model/converter/claim_converter.go
+++ internal/model/converter/claim_converter.go
@@ -2,7 +2,6 @@ package converter
 
 import (
 	"time" // Pastikan time diimport untuk time.Time{}
-
 	"github.com/thoriqwildan/aino-medical-be/internal/entity"
 	"github.com/thoriqwildan/aino-medical-be/internal/helper"
 	"github.com/thoriqwildan/aino-medical-be/internal/model"
@@ -12,7 +11,6 @@ func ClaimToResponse(claim *entity.Claim) *model.ClaimResponse {
 	var transactionDate helper.CustomDate
 	var submissionDate helper.CustomDate
 	var slaStatus string
-	var approvedAmount float64
 	var medicalFacility string
 	var city string
 	var diagnosis string
@@ -35,12 +33,6 @@ func ClaimToResponse(claim *entity.Claim) *model.ClaimResponse {
 		slaStatus = string(*claim.SLA)
 	} else {
 		slaStatus = ""
-	}
-
-	if claim.ApprovedAmount != nil {
-		approvedAmount = *claim.ApprovedAmount
-	} else {
-		approvedAmount = 0.0
 	}
 
 	if claim.MedicalFacilityName != nil {
@@ -79,7 +71,7 @@ func ClaimToResponse(claim *entity.Claim) *model.ClaimResponse {
 		TransactionDate:   transactionDate,
 		SubmissionDate:    submissionDate,
 		SLAStatus:         slaStatus,
-		ApprovedAmount:    approvedAmount,
+		ApprovedAmount:    claim.ApprovedAmount,
 		ClaimStatus:       string(claim.ClaimStatus),
 		MedicalFacility:   medicalFacility,
 		City:              city,
@@ -91,24 +83,28 @@ func ClaimToResponse(claim *entity.Claim) *model.ClaimResponse {
 	}
 
 	if claim.TransactionType != nil {
-		result.TransactionType = *TransactionTypeToResponse(claim.TransactionType)
+		result.TransactionType = TransactionTypeToResponse(claim.TransactionType)
 	}
 
 	if claim.Patient.ID != 0 {
-		result.Patient = model.PatientResponse{
-			ID:        claim.Patient.ID,
-			Name:      claim.Patient.Name,
-			BirthDate: helper.CustomDate(claim.Patient.BirthDate),
-			Gender:    string(claim.Patient.Gender),
+		result.Patient = &model.PatientResponse{
+			ID:                                claim.Patient.ID,
+			Name:                              claim.Patient.Name,
+			BirthDate:                         helper.CustomDate(claim.Patient.BirthDate),
+			Gender:                            string(claim.Patient.Gender),
+			PatientYearlyBenefitClaimResponse: []*model.PatientYearlyBenefitClaimResponse{},
 		}
+	}
+	if claim.Patient.PatientYearlyBenefitClaims != nil || len(claim.Patient.PatientYearlyBenefitClaims) > 0 {
+		result.Patient.PatientYearlyBenefitClaimResponse = PatientYearlyBenefitClaimToResponses(claim.Patient.PatientYearlyBenefitClaims)
 	}
 
 	if claim.Employee.ID != 0 {
 		result.Employee = EmployeeToResponse(&claim.Employee)
 	}
 
-	if claim.PatientBenefit.BenefitID != 0 { // PatientBenefit bukan pointer, cek BenefitID 0 adalah cara aman
-		result.Benefit = *BenefitToResponse(&claim.PatientBenefit.Benefit)
+	if claim.PatientBenefit.BenefitID != 0 {
+		result.Benefit = BenefitToResponse(&claim.PatientBenefit.Benefit)
 	}
 
 	return result
@@ -132,3 +128,48 @@ func PatientToResponse(patient *entity.Patient) *model.PatientResponse {
 	}
 	return result
 }
+
+// func ClaimImportRequestToEntity(claim *model.ClaimImportRequest) *entity.Claim {
+// 	claimEntity := &entity.Claim{
+// 		ID:                claim.ID,
+// 		PatientBenefitID:  claim.PatientID,
+// 		PatientID:         claim.PatientBenefitID,
+// 		EmployeeID:        claim.EmployeeID,
+// 		ClaimAmount:       claim.ClaimAmount,
+// 		TransactionTypeID: claim.TransactionTypeID,
+
+// 		ApprovedAmount:      claim.ApproveAmount,
+// 		ClaimStatus:         entity.ClaimStatus(claim.ClaimStatus),
+// 		MedicalFacilityName: claim.MedicalFacility,
+// 		City:                claim.City,
+// 		Diagnosis:           claim.Diagnosis,
+// 		DocLink:             claim.DocLink,
+// 		TransactionStatus:   entity.TransactionStatus(claim.TransactionStatus),
+// 	}
+// 	if claim.TransactionDate != nil {
+// 		claimEntity.TransactionDate = (*time.Time)(claim.TransactionDate)
+// 	}
+// 	if claim.SubmissionDate != nil {
+// 		claimEntity.SubmissionDate = (*time.Time)(claim.SubmissionDate)
+// 	}
+// 	if claim.SLA != nil {
+// 		claimEntity.SLA = func(value *model.ClaimImportRequest) *entity.SLA {
+// 			if value.SLA != nil {
+// 				slaStatus := helper.DetermineSLAStatus(*value.SLA)
+// 				return &slaStatus
+// 			} else {
+// 				return nil
+// 			}
+// 		}(claim)
+// 	}
+
+// 	return claimEntity
+// }
+
+// func ClaimsImportRequestToEntity(claims *model.ClaimsImportRequest) []*entity.Claim {
+// 	var claimsEntity []*entity.Claim
+// 	for _, claim := range claims.Claims {
+// 		claimsEntity = append(claimsEntity, ClaimImportRequestToEntity(claim))
+// 	}
+// 	return claimsEntity
+// }
