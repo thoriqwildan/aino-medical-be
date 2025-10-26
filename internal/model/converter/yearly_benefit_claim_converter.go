1c53e7a7f4ca72c6f8be5f64c058393914161209
tirtahakimpambudhi <awebthp@gmail.com>
2025-10-26 09:25:10 +0700

    chore: Update yearly_benefit_claim_converter.go files

--- internal/model/converter/yearly_benefit_claim_converter.go
+++ internal/model/converter/yearly_benefit_claim_converter.go
@@ -6,19 +6,26 @@ import (
 )
 
 func YearlyBenefitClaimToResponse(yearlyClaim *entity.YearlyBenefitClaim) *model.YearlyBenefitClaimResponse {
-	return &model.YearlyBenefitClaimResponse{
+	yearlyBenefitClaim := &model.YearlyBenefitClaimResponse{
 		ID:          yearlyClaim.ID,
 		Code:        yearlyClaim.Code,
 		YearlyClaim: yearlyClaim.YearlyClaim,
 		CreatedAt:   yearlyClaim.CreatedAt,
 		UpdatedAt:   yearlyClaim.UpdatedAt,
+		Benefits:    []*model.BenefitResponse{},
 	}
+	if yearlyClaim.Benefits != nil || len(yearlyBenefitClaim.Benefits) > 0 {
+		for _, benefit := range yearlyClaim.Benefits {
+			yearlyBenefitClaim.Benefits = append(yearlyBenefitClaim.Benefits, BenefitToResponse(benefit))
+		}
+	}
+	return yearlyBenefitClaim
 }
 
 func YearlyBenefitClaimToResponses(yearlyClaim []*entity.YearlyBenefitClaim) []*model.YearlyBenefitClaimResponse {
 	yearlyClaims := make([]*model.YearlyBenefitClaimResponse, len(yearlyClaim))
-	for _, yu := range yearlyClaim {
-		yearlyClaims = append(yearlyClaims, YearlyBenefitClaimToResponse(yu))
+	for i, yu := range yearlyClaim {
+		yearlyClaims[i] = YearlyBenefitClaimToResponse(yu)
 	}
 	return yearlyClaims
 }
