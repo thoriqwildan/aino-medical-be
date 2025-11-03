f82f280129b2e24f4ef995d9782eb2b0bf5fc592
tirtahakimpambudhi <awebthp@gmail.com>
2025-11-03 09:25:13 +0700

    chore: Update deparment_repository.go files

--- internal/repository/deparment_repository.go
+++ internal/repository/deparment_repository.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/sirupsen/logrus"
 	"github.com/thoriqwildan/aino-medical-be/internal/entity"
 	"github.com/thoriqwildan/aino-medical-be/internal/model"
@@ -16,6 +18,27 @@ func NewDepartmentRepository(log *logrus.Logger) *DepartmentRepository {
 	return &DepartmentRepository{
 		Log: log,
 	}
+}
+
+func (dr *DepartmentRepository) FindOrCreate(db *gorm.DB, department *entity.Department) (*entity.Department, error) {
+	var result entity.Department
+	errTake := db.Model(entity.Department{}).Where("name = ?", department.Name).Take(&result).Error
+	if errTake != nil && !errors.Is(errTake, gorm.ErrRecordNotFound) {
+		dr.Log.Error("Error when find department in method find or create", errTake.Error())
+		return nil, errTake
+	}
+
+	if errTake == nil {
+		return &result, nil 
+	}
+
+	errCreate := db.Create(department).Error
+	if errCreate != nil {
+		dr.Log.Error("Error when create department in method find or create")
+		return nil, errCreate
+	}
+
+	return department, nil
 }
 
 func (dr *DepartmentRepository) GetByName(db *gorm.DB, name string) error {
