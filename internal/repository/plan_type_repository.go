4fa7e9a99ae203257706b0b75bf3a1590b22a4e6
tirtahakimpambudhi <awebthp@gmail.com>
2025-11-07 09:25:15 +0700

    chore: Update plan_type_repository.go files

--- internal/repository/plan_type_repository.go
+++ internal/repository/plan_type_repository.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/sirupsen/logrus"
 	"github.com/thoriqwildan/aino-medical-be/internal/entity"
 	"github.com/thoriqwildan/aino-medical-be/internal/model"
@@ -16,6 +18,27 @@ func NewPlanTypeRepository(log *logrus.Logger) *PlanTypeRepository {
 	return &PlanTypeRepository{
 		Log: log,
 	}
+}
+
+func (ptr *PlanTypeRepository) FindOrCreate(db *gorm.DB, planType *entity.PlanType) (*entity.PlanType, error) {
+	var result entity.PlanType
+	errTake := db.Model(entity.PlanType{}).Where("name = ?", planType.Name).Take(&result).Error
+	if errTake != nil && !errors.Is(errTake, gorm.ErrRecordNotFound) {
+		ptr.Log.Error("Error when find plan type in method find or create", errTake.Error())
+		return nil, errTake
+	}
+
+	if errTake == nil {
+		return &result, nil 
+	}
+
+	errCreate := db.Create(planType).Error
+	if errCreate != nil {
+		ptr.Log.Error("Error when create plan type in method find or create")
+		return nil, errCreate
+	}
+
+	return planType, nil
 }
 
 func (ptr *PlanTypeRepository) FindByName(db *gorm.DB, name string) error {
