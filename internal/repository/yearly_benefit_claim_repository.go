fc4418af7655875f9f8a1b9c1bfba5cb60552658
tirtahakimpambudhi <awebthp@gmail.com>
2025-11-10 09:25:16 +0700

    chore: Update yearly_benefit_claim_repository.go files

--- internal/repository/yearly_benefit_claim_repository.go
+++ internal/repository/yearly_benefit_claim_repository.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/sirupsen/logrus"
@@ -15,7 +16,7 @@ type YearlyBenefitClaimRepository struct {
 }
 
 func NewYearlyBenefitClaimRepository(log *logrus.Logger) *YearlyBenefitClaimRepository {
-	return &YearlyBenefitClaimRepository{Log: log}
+	return &YearlyBenefitClaimRepository{Log: log, Repository: Repository[entity.YearlyBenefitClaim]{}}
 }
 
 func (r YearlyBenefitClaimRepository) GetAll(db *gorm.DB, request *model.YearlyBenefitClaimFilter) ([]*entity.YearlyBenefitClaim, int64, error) {
@@ -36,9 +37,30 @@ func (r YearlyBenefitClaimRepository) GetAll(db *gorm.DB, request *model.YearlyB
 	if err := base.Count(&count).Error; err != nil {
 		return nil, 0, fmt.Errorf("error when counting result find yearly benefit claims: %v", err)
 	}
-	if err := base.Find(&yearlyBenefitClaims).Error; err != nil {
+	if err := base.Preload("Benefits").Find(&yearlyBenefitClaims).Error; err != nil {
 		r.Log.WithField("repository", "YearlyBenefitClaimRepository").Errorf("get all yearly benefit claims failed: %v", err)
 		return nil, 0, err
 	}
 	return yearlyBenefitClaims, count, nil
+}
+
+func (r *YearlyBenefitClaimRepository) FindOrCreate(db *gorm.DB, yearlyBenefitClaim *entity.YearlyBenefitClaim) (*entity.YearlyBenefitClaim, error) {
+	var result entity.YearlyBenefitClaim
+	errTake := db.Model(entity.YearlyBenefitClaim{}).Where("name = ?", yearlyBenefitClaim.Code).Take(&result).Error
+	if errTake != nil && !errors.Is(errTake, gorm.ErrRecordNotFound) {
+		r.Log.Error("Error when find yearly benefit claim in method find or create", errTake.Error())
+		return nil, errTake
+	}
+
+	if errTake == nil {
+		return &result, nil 
+	}
+
+	errCreate := db.Create(yearlyBenefitClaim).Error
+	if errCreate != nil {
+		r.Log.Error("Error when create yearly benefit claim in method find or create")
+		return nil, errCreate
+	}
+
+	return yearlyBenefitClaim, nil
 }
